9f32aa7c2fd041d819e0087e2dc9230da77ff45e
williams123000 <williamschan72@gmail.com>
2024-02-13 17:56:09 -0600

    Se realiza el MVC Correctamente con la capa de Servicios, Adapter y Proxy

--- Main.py
+++ Main.py
@@ -20,22 +20,21 @@ logging.getLogger('flet_core').setLevel(logging.WARNING)
 # Se configura el nivel de registro de la aplicación.
 logging.basicConfig(filename='settings/Log.log',filemode='w', level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-if __name__ == "__main__":
 
-    # Proxy Login Connection
-    # Se utiliza Proxy_Login para autenticar el inicio de sesión.
-    Validate, ID_User = Proxy_Login()
+# Proxy Login Connection
+# Se utiliza Proxy_Login para autenticar el inicio de sesión.
+Validate, ID_User = Proxy_Login()
 
-    if Validate:
-        # Si la validación es exitosa, se asigna el ID de usuario global y se redirige a la vista principal de la aplicación.
-        logging.info("Inicio de sesion exitoso.")
-        # Importar la vista principal de la aplicación y la carga.
-        from View.GUI_Home import GUI_Home
-        logging.info("Carga de la vista principal de la aplicacion.")
-        ft.app(target=GUI_Home, assets_dir="assets")
-    else:
-        logging.error("Inicio de sesion fallido.")
-        # Si la validación falla, se muestra la vista de inicio de sesión para que el usuario vuelva a intentarlo.
-        logging.info("Carga de la vista de inicio de sesion.")
-        from View.GUI_Login import GUI_Login 
-        ft.app(target=GUI_Login, assets_dir="assets")
+if Validate:
+    # Si la validación es exitosa, se asigna el ID de usuario global y se redirige a la vista principal de la aplicación.
+    logging.info("Inicio de sesion exitoso.")
+    # Importar la vista principal de la aplicación y la carga.
+    from View.GUI_Home import GUI_Home
+    logging.info("Carga de la vista principal de la aplicacion.")
+    ft.app(target=GUI_Home, assets_dir="assets")
+else:
+    logging.error("Inicio de sesion fallido.")
+    # Si la validación falla, se muestra la vista de inicio de sesión para que el usuario vuelva a intentarlo.
+    logging.info("Carga de la vista de inicio de sesion.")
+    from View.GUI_Login import GUI_Login 
+    ft.app(target=GUI_Login, assets_dir="assets")
